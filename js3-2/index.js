11709062762c98cc67eefed0a24a22f73813c152
Ghostovich <gusarov_konstant@mail.ru>
2023-11-24 19:51:31 +0300

    js3-hw2.1

---
+++ js3-2/index.js
@@ -0,0 +1,32 @@
+const slider = document.querySelector('.container__img');
+const prevButton = document.querySelector('.buttons__prev');
+const nextButton = document.querySelector('.buttons__next');
+const slides = Array.from(slider.querySelectorAll('.img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+updateSlider();
+
